924b7bf2cddb828122f9f9d79a015637e8d6dd9a
DimitrijeDjukic <dimitrijedjkc@gmail.com>
2024-03-12 21:00:08 +0100

    feat: create AuthService signup

---
+++ src/users/auth.service.ts
@@ -0,0 +1,42 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { promisify } from "util";
+import { randomBytes, scrypt as _scrypt } from "crypto";
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) { }
+
+  async signup(email: string, password: string) {
+
+    // See if email is in use
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('email in use');
+    }
+
+    // Generate a salt 
+    const salt = randomBytes(8).toString('hex')
+
+    // Hash the salt and password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    // Join the hashed result and the salt together <- this is stored inside the db
+    const result = salt + '.' + hash.toString('hex');
+
+    // Create a new user and save it
+    const user = await this.usersService.create(email, result)
+
+    // Return the user
+    return user;
+
+
+  }
+
+  signin() {
+
+  }
+
+}
